8449f7c7b4392de47fcaa62ef5bcb3355fab83df
Matias Espince <m.espince@gmail.com>
2024-03-08 15:41:47 -0300

    Primera Pre Entrega

---
+++ main.js
@@ -0,0 +1,25 @@
+console.log("Bienvenido al Cotizador de Seguros de Matias Espince")
+
+function Persona(nombre, apellido, edad){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+}
+
+
+let nombre = prompt("Bienvenid@ ingrese su nombre");
+let apellido = prompt("Ingrese su Apellido");
+let edad = 2024 - parseFloat(prompt("Ingrese su año de nacimiento"));
+let valorPorAños = 350;
+
+const Persona1 = new Persona (nombre, apellido, edad)
+console.log(Persona1)
+
+alert ("Gracias por confiar en nosotros.");
+
+const cotizacion = ( edad * valorPorAños); 
+const cotiIva = (cotizacion * 1.21)
+
+
+console.log("hola " + nombre + " tu edad es " + edad + ", por lo cual tendras un seguro de valor $" + cotizacion + " pero al contar con IVA dicho valor seria de $" + cotiIva)
+
