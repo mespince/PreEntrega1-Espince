51cc96a92e14e9dead8b426e23bf1a3fea1ab31c
Matias Espince <m.espince@gmail.com>
2024-03-15 15:01:56 -0300

    segunda correccion

--- main.js
+++ main.js
@@ -5,28 +5,36 @@ function Persona(nombre, apellido, edad){
     this.apellido = apellido;
     this.edad = edad;
 }
-
-
-let nombre = prompt("Bienvenid@ ingrese su nombre");
-let apellido = prompt("Ingrese su Apellido");
-let edad = 2024 - parseFloat(prompt("Ingrese su año de nacimiento"));
 let valorPorAños = 350;
 
-const Persona1 = new Persona (nombre, apellido, edad)
-console.log(Persona1)
 
-alert ("Gracias por confiar en nosotros.");
+
+let nombre = prompt("Ingrese su nombre");
+while (nombre === "" || nombre === "."){
+    nombre = prompt("POR FAVOR INGRESE SU NOMBRE")
+}
+let apellido = prompt("Ingrese su apellido");
+while (apellido === "" || apellido === "."){
+    apellido = prompt("POR FAVOR INGRESE SU APELLIDO AHORA")
+}
+let año = parseFloat(prompt("Ingrese su año de nacimiento"));
+let edad = 2024 - año
+
+const Persona1 = new Persona (nombre, apellido, año)
+console.log(Persona1)
 
 const cotizacion = ( edad * valorPorAños); 
 const cotiIva = (cotizacion * 1.21)
 
-
-console.log("hola " + nombre + " tu edad es " + edad + ", por lo cual tendras un seguro de valor $" + cotizacion + " pero al contar con IVA dicho valor seria de $" + cotiIva)
-
-for (let i = 0; i <= 10; i++){
-    console.log(i);
+if(edad <=17){
+    alert ("Usted es menor de edad y no puede realizarse una cotizacion");
+}
+else{
+    console.log("hola " + nombre + " tu edad es " + edad + ", por lo cual tendras un seguro de valor $" + cotizacion + " pero al contar con IVA dicho valor seria de $" + cotiIva);
 }
 
-while (nombre != "Matias"){
-    console.log(nombre);
-}
+
+
+
+
+alert ("Gracias por confiar en nosotros.");
