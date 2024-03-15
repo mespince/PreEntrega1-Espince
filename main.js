561618da2551d8fc78030d5cf8ae9b7238107254
Matias Espince <m.espince@gmail.com>
2024-03-15 15:06:19 -0300

    no se subio bien lpm

--- main.js
+++ main.js
@@ -17,19 +17,27 @@ let apellido = prompt("Ingrese su apellido");
 while (apellido === "" || apellido === "."){
     apellido = prompt("POR FAVOR INGRESE SU APELLIDO AHORA")
 }
-let año = parseFloat(prompt("Ingrese su año de nacimiento"));
-let edad = 2024 - año
+;
 
+while (true) {
+    var año = prompt("Por favor ingrese su año de Nacimiento");
+    if (año.trim() !== "" && !isNaN(año)) {
+        var number = parseFloat(año);
+        break;
+    } else {
+        alert("ERROR! Ingrese un año Numerico.");
+    }
+}
 const Persona1 = new Persona (nombre, apellido, año)
 console.log(Persona1)
-
+let edad = 2024 - año
 const cotizacion = ( edad * valorPorAños); 
 const cotiIva = (cotizacion * 1.21)
-
 if(edad <=17){
     alert ("Usted es menor de edad y no puede realizarse una cotizacion");
 }
 else{
+    alert("hola " + nombre + " tu edad es " + edad + ", por lo cual tendras un seguro de valor $" + cotizacion + " pero al contar con IVA dicho valor seria de $" + cotiIva);
     console.log("hola " + nombre + " tu edad es " + edad + ", por lo cual tendras un seguro de valor $" + cotizacion + " pero al contar con IVA dicho valor seria de $" + cotiIva);
 }
 
